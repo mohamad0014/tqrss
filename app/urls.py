a3b2b70daf8e372a07befc8fbfa57e529f126adf
daniel.d <daniel.d@squarefish.ie>
2022-02-02 12:49:35 +0330

    first commit

---
+++ app/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('login', views.login, name='login'),
+    path('generate-plan', views.generate_plan, name='generate-plan'),
+    path('plans-list', views.plans_list, name='plans-list'),
+    path('compare-plan', views.compare_plan, name='compare-plan'),
+    path('release-plan', views.compare_plan, name='release-plan')
+]
---
+++ app/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return render(request, "main.html")
+
+
+def login(request):
+    return render(request, "login.html")
+
+
+def generate_plan(request):
+    return render(request, "generate-plan.html")
+
+
+def plans_list(request):
+    return render(request, "plans-list.html")
+
+
+def compare_plan(request):
+    return render(request, "compare-plan.html")
+
+
+def release_plan(request):
+    return render(request, "release-plan.html")
