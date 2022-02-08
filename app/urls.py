5f41a2f1b6b99d94f848ab2ca047f2bef791b633
daniel.d <daniel.d@squarefish.ie>
2022-02-08 21:52:22 +0330

    authentication user managmenet resolve some bugs

--- app/urls.py
+++ app/urls.py
@@ -3,11 +3,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('login/', views.loginPage, name='login'),
+    path('logout/', views.logoutUser, name="logout"),
+    path('user/add', views.add_user, name="add_user"),
+    path('user/list', views.user_list, name="user_list"),
     path('', views.index, name='index'),
-    path('login', views.login, name='login'),
-    path('generate-plan', views.generate_plan, name='generate-plan'),
-    path('review', views.review_plan, name='review_plan'),
-    path('plans-list', views.plans_list, name='plans-list'),
-    path('compare-plan', views.compare_plan, name='compare-plan'),
-    path('release-plan', views.compare_plan, name='release-plan')
+
+    path('generate-plan/', views.generate_plan, name='generate-plan'),
+    path('review/', views.review_plan, name='review_plan'),
+    path('plans-list/', views.plans_list, name='plans-list'),
+    path('compare-plan/', views.compare_plan, name='compare-plan'),
+    path('release-plan/', views.compare_plan, name='release-plan')
 ]
--- app/views.py
+++ app/views.py
@@ -1,13 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.forms import inlineformset_factory
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 
 def index(request):
     return render(request, "main.html")
 
 
-def login(request):
-    return render(request, "login.html")
+def loginPage(request):
+    context = {}
+    return render(request, 'login.html', context)
+    # if request.user.is_authenticated:
+    #     return redirect('/')
+    # else:
+    #     if request.method == 'POST':
+    #         username = request.POST.get('username')
+    #         password = request.POST.get('password')
+
+    #         user = authenticate(request, username=username, password=password)
+
+    #         if user is not None:
+    #             login(request, user)
+    #             return redirect('/')
+    #         else:
+    #             messages.info(request, 'Username OR password is incorrect')
+
+    #     context = {}
+    #     return render(request, 'login.html', context)
 
 
 def generate_plan(request):
@@ -28,3 +51,16 @@ def compare_plan(request):
 
 def release_plan(request):
     return render(request, "release-plan.html")
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
+
+
+def user_list(request):
+    return render(request, "users/list.html")
+
+
+def add_user(request):
+    return render(request, "users/add.html")
